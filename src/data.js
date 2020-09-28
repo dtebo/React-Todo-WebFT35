424dab82932eba688727b45042b89619cbcde26a
Darren Tebo <darrentebo83@outlook.com>
2020-09-28 15:08:18 -0400

    initial commit

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const todos = [
+    {
+        id: 1,
+        title: 'Wash Dishes',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'Get Groceries',
+        completed: false
+    }
+];
